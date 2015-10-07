3b0beeaaa5001468c7cc16680c5402bac9c09bd1
Nils Vollmer <nils_vollmer@gmx.de>
2015-10-07 17:33:35 +0200

    added some tests for parent section filter validation

--- test/models/category_test.rb
+++ test/models/category_test.rb
@@ -20,6 +20,10 @@ describe Category do
         category.expects(:validate_section_filter_presence)
         category.save
       end
+      it 'should have the same section filter as the parent' do
+        category.expects(:validate_section_filters_with_parent)
+        category.save
+      end
     end
   end
 
@@ -43,15 +47,38 @@ describe Category do
       end
     end
     describe '#validate_section_filter_presence' do
-      it 'should fail when there is no section filters' do
+      it 'should fail when there is no section filter' do
         category.expects(:fail_validation).with :section_filters,
                                                 'needs_section_filters'
         category.validate_section_filter_presence
       end
-      it 'should succeed when there is at least on section filter' do
+      it 'should succeed when there is at least one section filter' do
         category = categories(:main1)
         category.expects(:fail_validation).never
         category.validate_section_filter_presence
+      end
+    end
+    describe '#validate_section_filters_with_parent' do
+      it 'should fail when the parent does not have the section filter' do
+        parent = categories(:main1)
+        category = categories(:sub1)
+        section_filter = filters(:refugees)
+        category.expects(:fail_validation).with :section_filters,
+                                                'parent_needs_same_section_fil'\
+                                                'ter',
+                                                parent_name: parent.name,
+                                                filter_name: section_filter.name
+        category.validate_section_filters_with_parent
+      end
+      it 'should succeed when the parent has the same section filter' do
+        category = categories(:sub2)
+        category.expects(:fail_validation).never
+        category.validate_section_filters_with_parent
+      end
+      it 'should succeed on main categories (no parent)' do
+        category = categories(:main1)
+        category.expects(:fail_validation).never
+        category.validate_section_filters_with_parent
       end
     end
   end
