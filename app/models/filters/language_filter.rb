3b6150f87fdf0ed6526e2b4fc6a0bbf68320b714
Sebastian Schwiecker <sebastian.schwiecker@gmail.com>
2015-10-26 17:30:56 +0100

    Remove 'more languages' from frontend filter

--- app/models/filters/language_filter.rb
+++ app/models/filters/language_filter.rb
@@ -4,8 +4,8 @@ class LanguageFilter < Filter
   # identifier: ISO 639-2 code
 
   IDENTIFIER = %w(deu ara eng fra pol rus tur 150 326 568 639 amh aze ben bos
-                  bul ell fas hin hrv hun ita kur lav lit mkd mul por rom ron
-                  spa sqi srp tam tha urd vie wol
+                  bul ell fas hin hrv hun ita kur lav lit mkd por rom ron spa
+                  sqi srp tam tha urd vie wol
                 )
   enumerize :identifier, in: LanguageFilter::IDENTIFIER
 end
