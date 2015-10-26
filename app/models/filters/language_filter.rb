dd2e3ee958cae24c76a581027518d377e60d0231
Sebastian Schwiecker <sebastian.schwiecker@gmail.com>
2015-10-26 17:14:44 +0100

    Reorder languages fro frontend filter

--- app/models/filters/language_filter.rb
+++ app/models/filters/language_filter.rb
@@ -3,9 +3,9 @@ class LanguageFilter < Filter
   validates :identifier, length: { is: 3 }
   # identifier: ISO 639-2 code
 
-  IDENTIFIER = %w(
-    deu eng tur tha bul ron sqi ara fra kur rus srp amh bos ita hrv pol por vie
-    wol fas ben spa tam urd mul aze ell hin mkd rom hun 568 326 639 150 lav lit
-  )
+  IDENTIFIER = %w(deu ara eng fra pol rus tur 150 326 568 639 amh aze ben bos
+                  bul ell fas hin hrv hun ita kur lav lit mkd mul por rom ron
+                  spa sqi srp tam tha urd vie wol
+                )
   enumerize :identifier, in: LanguageFilter::IDENTIFIER
 end
