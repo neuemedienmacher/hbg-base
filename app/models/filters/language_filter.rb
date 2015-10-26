e60c28b811e070b01fde6eaea7a6a5223396f256
Nils Vollmer <nils_vollmer@gmx.de>
2015-10-26 17:48:36 +0100

    code style fix

--- app/models/filters/language_filter.rb
+++ app/models/filters/language_filter.rb
@@ -5,7 +5,6 @@ class LanguageFilter < Filter
 
   IDENTIFIER = %w(deu ara eng fra pol rus tur 150 sqi amh aze ben bos bul 639
                   ell hin ita hrv  kur lav lit mkd 568 fas por rom ron spa srp
-                  tam tha 326 hun urd vie wol
-                  )
+                  tam tha 326 hun urd vie wol)
   enumerize :identifier, in: LanguageFilter::IDENTIFIER
 end
