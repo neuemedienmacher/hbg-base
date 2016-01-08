d6490994dc93b60a437fa6c5284a9f57947c67e1
Nils Vollmer <nils_vollmer@gmx.de>
2016-01-08 15:52:50 +0100

    added missing language identifiers (sorted)

--- app/models/filters/language_filter.rb
+++ app/models/filters/language_filter.rb
@@ -3,8 +3,9 @@ class LanguageFilter < Filter
   validates :identifier, length: { is: 3 }
   # identifier: ISO 639-2 code
 
-  IDENTIFIER = %w(deu ara eng fra pol rus tur 150 sqi amh aze ben bos bul 639
-                  ell hin ita kik hrv kur lav lit mkd 568 fas por rom ron spa
-                  srp tam tha 326 hun urd vie wol)
+  IDENTIFIER = %w(deu ara eng fra pol rus tur 150 sqi amh aze ben bos bul zho
+                  639 ell hin ita kik hrv kur lav lit luo ltz mkd 568 nor fas
+                  por rom ron srp snk spa swa swe tgk tam tel tha tuk 326 hun
+                  urd uzb vie bel wol zza)
   enumerize :identifier, in: LanguageFilter::IDENTIFIER
 end
