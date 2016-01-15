9a6c7562e734a15f92c22f853b95c62412d927d7
Nils Vollmer <nils_vollmer@gmx.de>
2016-01-15 17:27:15 +0100

    fixed missing algolia reindex for certain after location_visible changes

--- app/observers/location_observer.rb
+++ app/observers/location_observer.rb
@@ -1,9 +1,15 @@
 class LocationObserver < ActiveRecord::Observer
-  # queue geocoding
   def after_save l
+    # queue geocoding
     if l.street_changed? || l.zip_changed? || l.city_changed? ||
        l.federal_state_id_changed?
       GeocodingWorker.perform_async l.id
+    end
+
+    # TODO: write Test for new LocationObserver functionality!!!
+    # update algolia indices of offers (for location_visible) if changed
+    if l.visible_changed?
+      l.offers.find_each(&:index!)
     end
   end
 end
