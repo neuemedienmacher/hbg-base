955511224e1c18fd8b9800149c7a78677b51b188
Nils Vollmer <nils_vollmer@gmx.de>
2016-01-22 15:05:24 +0100

    clarat-org/clarat#634 add age_visible to offers and and new target_audience_filters

--- app/models/filters/target_audience_filter.rb
+++ app/models/filters/target_audience_filter.rb
@@ -1,4 +1,9 @@
 class TargetAudienceFilter < Filter
-  IDENTIFIER = %w(children parents nuclear_family acquaintances pregnant_woman)
+  IDENTIFIER = %w(children parents nuclear_family acquaintances pregnant_woman
+                  everyone refugees refugees_children refugees_adolescents
+                  refugees refugees_children_and_adolescents refugees_umf
+                  refugees_ujf refugees_families refugees_pre_asylum_procedure
+                  refugees_asylum_procedure refugees_deportation_decision
+                  refugees_toleration_decision refugees_residence_permit)
   enumerize :identifier, in: TargetAudienceFilter::IDENTIFIER
 end
--- test/dummy/db/schema.rb
+++ test/dummy/db/schema.rb
@@ -11,7 +11,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20160115110729) do
+ActiveRecord::Schema.define(version: 20160121134028) do
 
   create_table "areas", force: :cascade do |t|
     t.string   "name",       null: false
@@ -235,6 +235,7 @@ ActiveRecord::Schema.define(version: 20160115110729) do
     t.string   "target_audience"
     t.string   "aasm_state",                 limit: 32
     t.boolean  "hide_contact_people",                    default: false
+    t.boolean  "age_visible",                            default: false
   end
 
   add_index "offers", ["aasm_state"], name: "index_offers_on_aasm_state"
