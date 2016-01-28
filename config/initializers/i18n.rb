1d57cd18d6df39c10923926289ff168d50d9a07c
Konstantin K <konstantin.koss@gmail.com>
2016-01-28 13:45:51 +0100

    only enable 4 languages for now

--- config/initializers/i18n.rb
+++ config/initializers/i18n.rb
@@ -1,12 +1,11 @@
 Rails.application.configure do
   I18n.config.enforce_available_locales = false
-  I18n.available_locales = [:de, :en, :ar, :fr, :pl, :tr, :ru]
+  I18n.available_locales = [:de, :en, :ar, :tr] # , :ru, :fr, :pl
 
   # The default locale is :en and all translations from config/locales/*.rb,yml are auto loaded.
   config.i18n.load_path += Dir[Rails.root.join('config', 'locales', '**', '*.{rb,yml}').to_s]
   config.i18n.load_path += Dir[ClaratBase::Engine.root.join('config', 'locales', '**', '*.{rb,yml}').to_s]
   config.i18n.default_locale = :de
-
 
   ### GLOBALIZE GEM ###
 
