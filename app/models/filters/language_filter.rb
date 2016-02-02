31fff747c62fe83d80bd5db044ab2b00def17e25
Nils Vollmer <nils_vollmer@gmx.de>
2016-02-02 15:31:35 +0100

    initial commit

--- app/models/filters/language_filter.rb
+++ app/models/filters/language_filter.rb
@@ -3,9 +3,9 @@ class LanguageFilter < Filter
   validates :identifier, length: { is: 3 }
   # identifier: ISO 639-2 code
 
-  IDENTIFIER = %w(deu ara eng fra pol rus tur 150 sqi amh aze ben bos bul zho
-                  639 ell hin ita kik hrv kur lav lit luo ltz mkd 568 nor fas
-                  por rom ron srp snk spa swa swe tgk tam tel tha tuk 326 hun
-                  urd uzb vie bel wol zza)
+  IDENTIFIER = %w(deu ara eng fra pol rus tur 150 sqi amh aze ban ben bos bul
+                  zho 639 ell heb fin hat hin ita kik hrv kur lav lit luo ltz
+                  mkd 568 nor fas por rom ron srp snk spa slo swa kor swe tgk
+                  tam tel tha cze tuk 326 hun urd uzb vie bel wol zza)
   enumerize :identifier, in: LanguageFilter::IDENTIFIER
 end
