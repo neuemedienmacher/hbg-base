19bed32c235d57b20f1c24e7cb4aad693bc2dd0d
Konstantin K <konstantin.koss@gmail.com>
2016-02-17 18:20:41 +0100

    for clarat-org/clarat#679 - add category per section filter validation

--- app/models/offer/validations.rb
+++ app/models/offer/validations.rb
@@ -39,26 +39,6 @@ class Offer
       #   validate_associated_fields
       #   validate_target_audience
       # end
-
-      private
-
-      # Uses method from CustomValidatable concern.
-      def validate_associated_fields
-        validate_associated_presence :organizations
-        validate_associated_presence :section_filters
-        validate_associated_presence :language_filters
-        if in_family_section?
-          validate_associated_presence :target_audience_filters
-        end
-      end
-
-      def validate_associated_presence field
-        fail_validation field, "needs_#{field}" if send(field).empty?
-      end
-
-      def in_family_section?
-        section_filters.to_a.any? { |filter| filter.identifier == 'family' }
-      end
     end
   end
 end
