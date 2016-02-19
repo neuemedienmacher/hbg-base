8bc474d8b94b4ec1d3eaf01cb9a0b829407417de
Nils Vollmer <nils_vollmer@gmx.de>
2016-02-19 16:47:41 +0100

    Hotfix: only apply last_version_id on new_records? after initialize

--- app/observers/offer_observer.rb
+++ app/observers/offer_observer.rb
@@ -1,7 +1,7 @@
 class OfferObserver < ActiveRecord::Observer
   def after_initialize offer
     offer.expires_at ||= (Time.zone.now + 1.year) if offer.new_record?
-    offer.logic_version_id = LogicVersion.last.id
+    offer.logic_version_id = LogicVersion.last.id if offer.new_record?
   end
 
   def after_save offer
