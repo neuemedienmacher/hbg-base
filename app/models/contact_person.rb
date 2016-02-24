40ee42011f21460f225bdb8d540c942846c2cd31
Nils Vollmer <nils_vollmer@gmx.de>
2016-02-24 10:47:38 +0100

    mini for clarat-org/clarat#703 - remove secretary from positions

--- app/models/contact_person.rb
+++ app/models/contact_person.rb
@@ -15,7 +15,7 @@ class ContactPerson < ActiveRecord::Base
   extend Enumerize
   enumerize :gender, in: %w(female male)
   enumerize :academic_title, in: %w(dr prof_dr)
-  enumerize :position, in: %w(superior public_relations secretary other)
+  enumerize :position, in: %w(superior public_relations other)
 
   # Validations
   validates :organization_id, presence: true
