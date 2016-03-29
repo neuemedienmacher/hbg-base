7ed28076755d4ec0fd01f2f133cfc28881424896
Sebastian Schwiecker <sebastian.schwiecker@gmail.com>
2016-03-29 16:33:46 +0200

    Add more languages available on request identifier #706

--- app/models/filters/language_filter.rb
+++ app/models/filters/language_filter.rb
@@ -7,10 +7,10 @@ class LanguageFilter < Filter
   FIXED_IDENTIFIER = %w(deu ara eng fra pol rus tur 150).freeze
   # sorted by german display name
   REMAINING_IDENTIFIER = %w(sqi amh aze ban ben bos bul zho 639 fin ell heb hin
-                            ita kik kor hrv kur lav lit luo ltz mkd hbs 568 nor
-                            fas por rom ron srp som snk slo spa swa swe tgk tam
-                            tel tha cze tir tuk 326 hun urd uzb vie bel wol zza
-                            ).freeze
+                            ita kik kor hrv kur lav lit luo ltz mkd mul hbs 568
+                            nor fas por rom ron srp som snk slo spa swa swe tgk
+                            tam tel tha cze tir tuk 326 hun urd uzb vie bel wol
+                            zza).freeze
   IDENTIFIER = FIXED_IDENTIFIER + REMAINING_IDENTIFIER
   enumerize :identifier, in: LanguageFilter::IDENTIFIER
 end
