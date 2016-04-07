646da1c685f22073d98289d6bb012e32fa70c676
Nils Vollmer <nils_vollmer@gmx.de>
2016-04-07 10:22:35 +0200

    changed order in enum

--- app/models/filters/target_audience_filter.rb
+++ app/models/filters/target_audience_filter.rb
@@ -5,8 +5,8 @@ class TargetAudienceFilter < Filter
 
   # TODO: remove family_pregnant_woman after conversion of data!!
   FAMILY_IDENTIFIER = %w(family_children family_parents family_nuclear_family
-                         family_acquaintances family_everyone
-                         family_parents_to_be).freeze
+                         family_acquaintances family_parents_to_be
+                         family_everyone).freeze
   REFUGEES_IDENTIFIER =
     %w(refugees_children refugees_adolescents refugees_children_and_adolescents
        refugees_umf refugees_ujf refugees_families refugees_pre_asylum_procedure
