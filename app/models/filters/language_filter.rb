cabf276fa72fb797980573e37208e9b6b9c5ca42
Nils Vollmer <nils_vollmer@gmx.de>
2016-04-29 12:09:17 +0200

    clarat-org/clarat#785 new language filters

--- app/models/filters/language_filter.rb
+++ app/models/filters/language_filter.rb
@@ -6,11 +6,11 @@ class LanguageFilter < Filter
   # fixed sort (by importance)
   FIXED_IDENTIFIER = %w(deu ara eng fra pol rus tur 150).freeze
   # sorted by german display name
-  REMAINING_IDENTIFIER = %w(sqi amh aze ban ben bos bul zho 639 fin ell heb hin
-                            ita kik kor hrv kur lav lit luo ltz mkd mul hbs 568
-                            nor fas por rom ron srp som snk slo spa swa swe tgk
-                            tam tel tha cze tir tuk 326 hun urd uzb vie bel wol
-                            zza).freeze
+  REMAINING_IDENTIFIER = %w(sqi amh aze ban ben ber bos bul zho 639 fin ell heb
+                            hin ita kik kin kor hrv kur lav lit luo ltz mkd mul
+                            hbs 568 nor fas por rom ron srp som snk slo spa swa
+                            swe tgk tgl tam tel tha cze tir tuk 326 hun urd uzb
+                            vie bel wol zza).freeze
   IDENTIFIER = FIXED_IDENTIFIER + REMAINING_IDENTIFIER
   enumerize :identifier, in: LanguageFilter::IDENTIFIER
 end
