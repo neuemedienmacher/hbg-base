12e87620e5d79421cd53ee629368cd07c374dcd1
Nils Vollmer <nils_vollmer@gmx.de>
2016-06-14 15:48:14 +0200

    clarat-org/clarat#822 - orga translation only when fields changed and after complete event

--- test/models/organization_test.rb
+++ test/models/organization_test.rb
@@ -339,12 +339,49 @@ describe Organization do
   end
 
   describe 'translation' do
-    it 'should always get de translation, others only when not initialized' do
+    it 'should always get de translation, others only on completion and change' do
       new_orga = FactoryGirl.create(:organization)
       new_orga.translations.count.must_equal 1 # only :de
-      new_orga.update_columns aasm_state: 'completed'
-      OrganizationObserver.send(:new).after_save(new_orga)
+      new_orga.translations.first.locale.must_equal 'de'
+      new_orga.aasm_state.must_equal 'initialized'
+
+      # Changing things on an initialized offer doesn't change translations
+      new_orga.reload.description_ar.must_equal nil
+      new_orga.description = 'changing description, wont update translation'
+      new_orga.save!
+      new_orga.translations.count.must_equal 1
+      new_orga.reload.description_ar.must_equal nil
+
+      # Completion generates all translations initially
+      new_orga.complete!
       new_orga.translations.count.must_equal I18n.available_locales.count
+
+      # Now changes to the model change the corresponding translated fields
+      EasyTranslate.translated_with 'CHANGED' do
+        new_orga.description_ar.must_equal 'GET READY FOR CANADA'
+        new_orga.description = 'changing description, should update translation'
+        new_orga.save!
+        new_orga.reload.description_ar.must_equal 'CHANGED'
+      end
+    end
+
+    it 'should update an existing translation only when the field changed' do
+      # Setup
+      new_orga = FactoryGirl.create(:organization)
+      new_orga.complete!
+      new_orga.translations.count.must_equal I18n.available_locales.count
+
+      # Now changes to the model change the corresponding translated fields
+      EasyTranslate.translated_with 'CHANGED' do
+        new_orga.description_ar.must_equal 'GET READY FOR CANADA'
+        # changing untranslated field => translations must stay the same
+        new_orga.mailings_enabled = true
+        new_orga.save!
+        new_orga.reload.description_ar.must_equal 'GET READY FOR CANADA'
+        new_orga.description = 'changing descr, should update translation'
+        new_orga.save!
+        new_orga.reload.description_ar.must_equal 'CHANGED'
+      end
     end
   end
 
