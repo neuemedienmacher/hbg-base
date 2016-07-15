6836fcc87febaa4643402b84dd5cae704833d079
Nils Vollmer <nils_vollmer@gmx.de>
2016-07-15 09:53:20 +0200

    remove unneeded code

--- db/migrate/20160715072445_change_participant_structure_enum_names.rb
+++ db/migrate/20160715072445_change_participant_structure_enum_names.rb
@@ -1,12 +1,5 @@
 class ChangeParticipantStructureEnumNames < ActiveRecord::Migration
   def change
-    # TODO: needed or problematic?
-    # class Offer < ActiveRecord::Base
-    #   extend Enumerize
-    #   enumerize :participant_structure, in: %w(target_audience_alone
-    #     target_audience_and_others target_audience_and_others_with_same_problem)
-    # end
-
     Offer.where(participant_structure: 'target_audience_and_others').
       update_all(participant_structure: 'target_audience_in_group_with_others_with_different_problems')
 
