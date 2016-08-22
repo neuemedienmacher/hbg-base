801b6a2fd136ca76aa2a8d82be983887180987f5
Sebastian Schwiecker <sebastian.schwiecker@gmail.com>
2016-08-22 14:25:14 +0200

    Cleanup

--- app/models/filters/language_filter.rb
+++ app/models/filters/language_filter.rb
@@ -3,14 +3,7 @@ class LanguageFilter < Filter
   validates :identifier, length: { is: 3 }, presence: true, uniqueness: true
   # identifier: ISO 639-2 code (+ additional values)
 
-  # fixed sort (by importance)
-  FIXED_IDENTIFIER = %w(deu rom).freeze
   # sorted by german display name
-  REMAINING_IDENTIFIER = %w(sqi amh aze ban ben ber bos bul zho 639 fin ell heb
-                            hin ita kik kin kor hrv kur lav lit luo ltz mkd hbs
-                            568 nor pus fas por rom ron srp skr som snk slo spa
-                            swa swe tgk tgl tam tel tha cze tir tuk twi 326 hun
-                            urd uzb vie bel wol zza mul).freeze
   IDENTIFIER = %w(sqi amh ara aze ban ben ber bos bul zho 639 deu 150 eng fin
                   fra ell heb hin ita kik kin kor hrv kur lav lit luo ltz mkd
                   hbs 568 nor pus fas pol por rom ron srp skr som snk slo spa
