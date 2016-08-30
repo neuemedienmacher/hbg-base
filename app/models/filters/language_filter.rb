292211622f2eded21146672b13b9d5d38e01d0ad
Nils Vollmer <nils_vollmer@gmx.de>
2016-08-30 17:37:56 +0200

    clarat-org/clarat#900 - added two new language enums

--- app/models/filters/language_filter.rb
+++ app/models/filters/language_filter.rb
@@ -4,10 +4,10 @@ class LanguageFilter < Filter
   # identifier: ISO 639-2 code (+ additional values)
 
   # sorted by german display name
-  IDENTIFIER = %w(sqi amh ara aze ban ben ber bos bul zho 639 deu 150 eng fin
-                  fra ell heb hin ita kik kin kor hrv kur lav lit luo ltz mkd
-                  hbs 568 nor pus fas pol por rom ron srp skr som snk slo spa
-                  swa rus swe tgk tgl tam tel tha tir cze tur tuk twi 326 hun
-                  urd uzb vie bel wol zza mul).freeze
+  IDENTIFIER = %w(sqi amh ara aze ban ben ber bos bul zho 639 deu 150 eng ewo
+                  fin fra ell heb hin ita cpe kik kin kor hrv kur lav lit luo
+                  ltz mkd hbs 568 nor pus fas pol por rom ron srp skr som snk
+                  slo spa swa rus swe tgk tgl tam tel tha tir cze tur tuk twi
+                  326 hun urd uzb vie bel wol zza mul).freeze
   enumerize :identifier, in: LanguageFilter::IDENTIFIER
 end
