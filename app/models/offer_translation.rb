92fd2355fdba58c5ad51932ffe5ed92b05c0ad1e
Nils Vollmer <nils_vollmer@gmx.de>
2016-09-30 10:28:16 +0200

    clarat-org/clarat#919 - moved associations to _base and made section_filters on organizations uniq

--- app/models/offer_translation.rb
+++ app/models/offer_translation.rb
@@ -1,5 +1,7 @@
 class OfferTranslation < ActiveRecord::Base
   include BaseTranslation
 
+  # Associations
   belongs_to :offer, inverse_of: :translations
+  has_many :section_filters, through: :offer
 end
--- app/models/organization_translation.rb
+++ app/models/organization_translation.rb
@@ -1,5 +1,7 @@
 class OrganizationTranslation < ActiveRecord::Base
   include BaseTranslation
 
+  # Associations
   belongs_to :organization, inverse_of: :translations
+  has_many :section_filters, through: :organization
 end
