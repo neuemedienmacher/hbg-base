544f04717cd4075009f013d31a5f4afcb4280492
Sebastian Schwiecker <sebastian.schwiecker@gmail.com>
2016-10-04 12:09:47 +0200

    Add new TraitFilter

--- app/models/filters/trait_filter.rb
+++ app/models/filters/trait_filter.rb
@@ -1,5 +1,6 @@
 # Additional traits
 class TraitFilter < Filter
-  IDENTIFIER = %w(anonymous free confidential day_and_night charged).freeze
+  IDENTIFIER = %w(anonymous free confidential day_and_night charged
+                  accessibility).freeze
   enumerize :identifier, in: TraitFilter::IDENTIFIER
 end
