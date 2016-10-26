fbaf94d9826829e182bfc1b2bbd6d580d388b743
Nils Vollmer <nils_vollmer@gmx.de>
2016-10-26 10:23:48 +0200

    Fixing yet another problematic (not uniq) has_many through association

--- app/models/city.rb
+++ app/models/city.rb
@@ -3,7 +3,7 @@ class City < ActiveRecord::Base
   # Associations
   has_many :locations, inverse_of: :city
   has_many :offers, through: :locations
-  has_many :organizations, through: :locations
+  has_many :organizations, -> { uniq }, through: :locations
 
   # Validations
   validates :name, uniqueness: true, presence: true
