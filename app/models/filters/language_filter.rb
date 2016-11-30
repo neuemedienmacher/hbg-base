13603b6e9b1001f4495e11905ba7f6a05c912b15
Nils Vollmer <nils_vollmer@gmx.de>
2016-11-30 10:47:29 +0100

    mini-feature for clarat-org/clarat#946 and clarat-org/clarat#958 -  added new language codes

--- app/models/filters/language_filter.rb
+++ app/models/filters/language_filter.rb
@@ -4,10 +4,11 @@ class LanguageFilter < Filter
   # identifier: ISO 639-2 code (+ additional values)
 
   # sorted by german display name
-  IDENTIFIER = %w(sqi amh ara aze ban bam ben ber bos bul zho 639 deu 150 dyu
-                  eng ewo fin fra ell heb hin ita cpe kik kin kor hrv kur lav
-                  lit luo ltz man mkd hbs 568 nor pus fas pol por rom ron srp
-                  skr slo slv som snk spa swa rus swe tgk tgl tam tel tha tir
-                  cze tur tuk twi 326 hun urd uzb vie bel wol zza mul).freeze
+  IDENTIFIER = %w(cau sqi amh ara arc aze ban bam ben ber bos bul zho 639 deu
+                  150 dyu eng ewo fin fra ell heb hin ita cpe kik kin kor hrv
+                  kur lav lit luo ltz man mkd hbs 568 nor pus fas pol por rom
+                  ron srp skr slo slv som snk spa swa rus swe tgk tgl tam tel
+                  tha tir cze tur tuk twi 326 hun urd uzb vie bel wol zza mul
+                  ).freeze
   enumerize :identifier, in: LanguageFilter::IDENTIFIER
 end
