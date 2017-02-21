cce8a2451cbe54d1fbec71a0f26a04c6a54ba560
allthegoldt <damjangoldt@Damjans-MacBook-Air.local>
2017-02-21 11:18:14 +0100

    contact-person-translations-migration

--- db/migrate/20170220153038_add_translation_to_contact_people_responsibility.rb
+++ db/migrate/20170220153038_add_translation_to_contact_people_responsibility.rb
@@ -1,7 +1,7 @@
 class AddTranslationToContactPeopleResponsibility < ActiveRecord::Migration
   def change
 
-    create_table "contact_people_translations", force: true do |t|
+    create_table "contact_person_translations", force: true do |t|
       t.integer  "contact_people_id",              null: false
       t.string   "locale",                       null: false
       t.string   "source",          default: "", null: false
@@ -10,8 +10,8 @@ class AddTranslationToContactPeopleResponsibility < ActiveRecord::Migration
       t.text     "responsibility",     default: "", null: false
     end
 
-    add_index "contact_people_translations", ["locale"], name: "index_contact_people_translations_on_locale"
-    add_index "contact_people_translations", ["contact_people_id"], name: "index_contact_people_translations_on_contact_people_id"
+    add_index "contact_person_translations", ["locale"], name: "index_contact_person_translations_on_locale"
+    add_index "contact_person_translations", ["contact_people_id"], name: "index_contact_person_translations_on_contact_people_id"
 
   end
 end
