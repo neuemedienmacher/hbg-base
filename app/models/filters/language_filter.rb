131efc78a44bec511033d308a67e59b4372d3b04
omaralsabbagh <omar.guitar92@gmail.com>
2017-04-12 17:46:10 +0200

    Die neuen Sprachen vom #1070

--- app/models/filters/language_filter.rb
+++ app/models/filters/language_filter.rb
@@ -4,9 +4,9 @@ class LanguageFilter < Filter
   # identifier: ISO 639-2 code (+ additional values)
 
   # sorted by german display name
-  IDENTIFIER = %w(cau aar sqi abk amh ara arc aze ban bam ben ber bos bul zho
-                  639 deu 150 dyu eng ewo fin fra ell heb hin ita cpe kik kin
-                  kor hrv kur lav lit luo ltz msa man mkd hbs 568 nor pan pus
+  IDENTIFIER = %w(cau aar sqi abk amh ara arc arm aze ban bam ben ber bos bul zho
+                  639 deu 150 dyu dan eng ewo fin fra ell hau heb hin ita cpe kik kin
+                  kor hrv kur lav lin lit luo ltz msa man mkd hbs mos 568 nor pan pus
                   fas pol por fuf rom ron srp skr slo slv som snk ckb spa swa
                   rus swe cus tgk tgl tam tel tha tig tir cze tur tuk twi 326
                   hun urd uzb vie bel wol zza mul).freeze
