3602a6a2126e7592306644071124853ec4cd3afc
Omar Alsabbagh <omar.guitar92@gmail.com>
2017-04-19 16:01:46 +0200

    some work for clarat-org/clarat#1070 more text added 6 more languages (#95)
    * some work for clarat-org/clarat#1070 more text added 6 more languages
    
    * some work 2 for clarat-org/clarat#1070 more text added 6 more languages
    
    * clarat-org/clarat#1070 more text added 6 more languages ans some code organizing

--- app/models/filters/language_filter.rb
+++ app/models/filters/language_filter.rb
@@ -2,13 +2,12 @@ class LanguageFilter < Filter
   # Validations
   validates :identifier, length: { is: 3 }, presence: true, uniqueness: true
   # identifier: ISO 639-2 code (+ additional values)
-
   # sorted by german display name
-  IDENTIFIER = %w(cau aar sqi abk amh ara arc arm aze ban bam ben ber bos bul zho
-                  639 deu 150 dyu dan eng ewo fil fin fra ell hau heb hin ita cpe kik kin
-                  kor hrv kur lav lin lit luo ltz msa man mkd hbs mos 568 nor pan pus
-                  fas pol por fuf rom ron srp skr slo slv som snk ckb spa swa
-                  rus swe cus tgk tgl tam tel tha tig tir cze tur tuk twi 326
-                  hun urd uzb vie bel wol zza mul).freeze
+  IDENTIFIER = %w(cau aar sqi abk amh ara arc arm aze ban bam ben ber bos bul
+                  zho 639 deu 150 dyu dan eng ewo fil fin fra ell hau heb hin 
+                  ita cpe kik kin kor hrv kur lav lin lit luo ltz msa man mkd
+                  hbs mos 568 nor pan pus fas pol por fuf rom ron srp skr slo
+                  slv som snk ckb spa swa rus swe cus tgk tgl tam tel tha tig
+                  tir cze tur tuk twi 326 hun urd uzb vie bel wol zza mul).freeze
   enumerize :identifier, in: LanguageFilter::IDENTIFIER
 end
