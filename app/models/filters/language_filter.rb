a85496705134437c2f9db83724c3ddf422d9fc35
Nils Vollmer <nils_vollmer@gmx.de>
2017-04-19 17:03:33 +0200

    removed trailing whitespace

--- app/models/filters/language_filter.rb
+++ app/models/filters/language_filter.rb
@@ -4,7 +4,7 @@ class LanguageFilter < Filter
   # identifier: ISO 639-2 code (+ additional values)
   # sorted by german display name
   IDENTIFIER = %w(cau aar sqi abk amh ara arc arm aze ban bam ben ber bos bul
-                  zho 639 deu 150 dyu dan eng ewo fil fin fra ell hau heb hin 
+                  zho 639 deu 150 dyu dan eng ewo fil fin fra ell hau heb hin
                   ita cpe kik kin kor hrv kur lav lin lit luo ltz msa man mkd
                   hbs mos 568 nor pan pus fas pol por fuf rom ron srp skr slo
                   slv som snk ckb spa swa rus swe cus tgk tgl tam tel tha tig
