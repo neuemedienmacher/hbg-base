010a76d0281da809590a3d22d73325dd1a3dcb3f
mokus80 <felhoesi@clarat.org>
2017-04-25 10:23:13 +0200

    delete migration file

--- db/migrate/20170424081649_rename_section_filters_to_sections.rb
+++ db/migrate/20170424081649_rename_section_filters_to_sections.rb
@@ -1,8 +1,8 @@
 class RenameSectionFiltersToSections < ActiveRecord::Migration
   def change
     rename_table :section_filters, :sections
-    rename_table :categories_section_filters, :categories_sections
-    rename_column :categories_sections, :section_filter_id, :section_id
+    rename_table :categories_filters, :categories_sections
+    rename_column :categories_sections, :filter_id, :section_id
     rename_column :divisions, :section_filter_id, :section_id
     rename_column :filters, :section_filter_id, :section_id
     rename_column :offers, :section_filter_id, :section_id
@@ -12,4 +12,10 @@ class RenameSectionFiltersToSections < ActiveRecord::Migration
     add_index :divisions, :section_id
     add_index :offers, :section_id
   end
+
+  CategoriesSection.find_each do |c|
+    new_id = c.filter_id == 76 ? 1 : 2
+    c.update_columns id: new_id
+  end
+
 end
