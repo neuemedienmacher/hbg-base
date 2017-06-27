39085d10b08a80276f60af9d57b5bc442b9d7f21
Nils Vollmer <nils_vollmer@gmx.de>
2017-06-27 16:14:03 +0200

    changes conditional migration for heroku deploy (Rails.configuration.database_configuration is nil while deploying to heroku)

--- db/migrate/20170626122106_install_trigram_search.rb
+++ db/migrate/20170626122106_install_trigram_search.rb
@@ -1,12 +1,14 @@
 class InstallTrigramSearch < ActiveRecord::Migration
   def up
-    if Rails.configuration.database_configuration[Rails.env]['adapter'] == 'postgresql'
+    if Rails.env.production? || Rails.env.staging? ||
+       Rails.configuration.database_configuration[Rails.env]['adapter'] == 'postgresql'
       execute "CREATE EXTENSION pg_trgm;"
     end
   end
 
   def down
-    if Rails.configuration.database_configuration[Rails.env]['adapter'] == 'postgresql'
+    if Rails.env.production? || Rails.env.staging? ||
+       Rails.configuration.database_configuration[Rails.env]['adapter'] == 'postgresql'
       execute "DROP EXTENSION pg_trgm;"
     end
   end
