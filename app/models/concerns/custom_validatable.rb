d6358562ef28810931cf00cb1f6ecf7c439b3106
mokus80 <felhoesi@clarat.org>
2017-07-24 13:07:45 +0200

    more updates, WIP

--- app/models/concerns/custom_validatable.rb
+++ app/models/concerns/custom_validatable.rb
@@ -3,10 +3,10 @@ module CustomValidatable
 
   included do
     def fail_validation field, i18n_selector, options = {}
-      errors[field] =
-        I18n.t(
+      errors.add(field, I18n.t(
           "#{self.class.name.downcase}.validations.#{i18n_selector}", options
         )
+      )
     end
   end
 end
--- test/support/spec_helpers/brakeman.rb
+++ test/support/spec_helpers/brakeman.rb
@@ -1,5 +1,4 @@
 require 'json'
-require 'colored'
 
 # Check security
 def brakeman
