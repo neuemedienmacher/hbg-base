992bdfcd143d1761cd18954ddbe71419faec1b76
Konstantin K <konstantin.koss@gmail.com>
2017-08-14 14:08:53 +0200

    hotfix: orga <-> section association goes through divisions, not offers

--- app/models/section.rb
+++ app/models/section.rb
@@ -1,8 +1,9 @@
 class Section < ActiveRecord::Base
   has_many :offers
-  has_many :organizations, -> { uniq }, through: :offers, inverse_of: :sections
-  has_many :target_audience_filters, inverse_of: :section
   has_many :divisions, inverse_of: :section
+  has_many :organizations, -> { uniq },
+           through: :divisions, inverse_of: :sections
+  has_many :target_audience_filters, inverse_of: :section
   has_many :categories_sections
   has_many :categories, through: :categories_sections
   has_many :cities, -> { uniq }, through: :offers, inverse_of: :sections
