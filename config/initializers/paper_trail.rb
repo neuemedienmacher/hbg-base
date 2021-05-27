9db1ad1903d24926217c13df691f4beff3a02021
Robert Lanzke <winkelement@hotmail.com>
2021-05-27 19:29:55 +0200

    do not call call track_associations at all

--- config/initializers/paper_trail.rb
+++ config/initializers/paper_trail.rb
@@ -1,1 +1,2 @@
-PaperTrail.config.track_associations = false
+#PaperTrail.config.track_associations = false
+# See https://github.com/paper-trail-gem/paper_trail/blob/49659e8a0c4cb0c80aeab344b7b434de7f057c88/CHANGELOG.md#breaking-changes-3
